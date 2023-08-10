31aabe3aff980d8fa28b4dd872addc75a6a03eaa
jaredden <james.ifit@gmail.com>
2023-08-10 13:54:45 -0400

    created for show page

--- controllers/foods.js
+++ controllers/foods.js
@@ -1,24 +1,31 @@
-const Food = require("../models/food")
+const Food = require("../models/food");
+
 module.exports = {
-    index,
-    new: newFood,
-    show, 
-    create, 
+  index,
+  new: newFood,
+  show,
+  create,
+};
+
+async function index(req, res) {
+  res.render("foods/index", { title: "All Foods", foods });
 }
 
-async function index(req,res){
-res.render('foods/index', {title: 'All Foods', foods})
+function newFood(req, res) {
+  res.render("foods/new", { title: "New Food", errorMsg: "" });
 }
 
-function newFood(req,res){
-    res.render('foods/new', {title: 'New Food', errorMsg: ''})
+async function show(req, res) {
+  res.render("foods/show", { title: "Food Details", food });
 }
 
+async function create(req, res) {
+  try {
+    await Food.create(req.body);
+    res.redirect("/food");
+  } catch {
+    err;
 
-async function show(req,res){    
-    res.render('foods/show', {title: 'Food Details', food})
-}
-
-async function create(req,res){ 
-    res.render('foods/new', {errorMsg: err.message})
+    res.render("foods/new", { errorMsg: err.message });
+  }
 }
