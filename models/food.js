5d0746c086595ae1e15a95eab47e9fdd12ed87c4
jhsu79 <joshuanhsu@gmail.com>
2023-08-10 12:33:51 -0400

    steps 3-6

---
+++ models/food.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const foodSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    category: {
+      type: String,
+      enum: [
+        "American",
+        "Chinese",
+        "Thai",
+        "Japanese",
+        "Italian",
+        "Mexican",
+        "Indian",
+      ],
+    },
+    favorite: { type: Boolean, default: false },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Food", foodSchema);
